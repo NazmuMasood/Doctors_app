95ce9303d7ebd7d283736df9b9ba7a0768de4426
Amir Faiyaz <42873787+amfam@users.noreply.github.com>
2020-07-31 20:17:48 +0600

    Add files via upload

---
+++ lib/screens/SignupPage.dart
@@ -0,0 +1,209 @@
+
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:doctors_app/screens/login.dart';
+
+
+class SignupPage extends StatefulWidget {
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  Signupmodel signupmodel = Signupmodel();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  FirebaseAuth _firebaseAuth= FirebaseAuth.instance;
+
+
+  save(){  //form saving 
+    if(_formKey.currentState.validate()){
+      _formKey.currentState.save();
+      _firebaseAuth.createUserWithEmailAndPassword( //firebase auth using email & password
+        email: signupmodel.email,
+        password: signupmodel.confirmPassword);
+      print(signupmodel.name);
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginPage()));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        resizeToAvoidBottomPadding: true,
+        body: Column(crossAxisAlignment: CrossAxisAlignment.start,
+         children: <Widget>[
+          Container(
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                  child: Text(
+                    'Signup',
+                    style:
+                        TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(260.0, 125.0, 0.0, 0.0),
+                  child: Text(
+                    '.',
+                    style: TextStyle(
+                        fontSize: 80.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+              padding: EdgeInsets.only(top: 15.0, left: 30.0, right: 30.0),
+              child:Form(
+              key: _formKey,//therjke
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                     validator: (input) {
+                      if(input.isEmpty){
+                    return 'Please type your name';
+                  }
+                  return null;
+                },
+                    onSaved: (input) => signupmodel.name=input,
+                    decoration: InputDecoration(
+                        labelText: 'NAME',
+                        labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                        // hintText: 'EMAIL',
+                        // hintStyle: ,
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green))),
+                  ),
+                  SizedBox(height: 10.0),
+                  TextFormField(
+                    validator: (input) {
+                      if(input.isEmpty){
+                    return 'Please type an email';
+                  }
+                  return null;
+                },
+                    onSaved: (input) => signupmodel.email=input,
+                    decoration: InputDecoration(
+                        labelText: 'EMAIL ',
+                        labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green))),
+                    
+                  ),
+                  SizedBox(height: 10.0),
+                  TextFormField(
+                    validator: (input){
+                  if(input.isEmpty){
+                    return 'Please provide a email';
+                  }
+                  return null;
+                },
+                    onSaved: (input) => signupmodel.password=input,
+                    decoration: InputDecoration(
+                        labelText: 'PASSWORD ',
+                        labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green))),
+                            obscureText: true,
+                  ),
+              
+                  SizedBox(height: 10.0),
+                  TextFormField(
+                    validator: (input){
+                  if(input.isEmpty){
+                    return 'Please provide a password';
+                  }
+                  if(input.length<6){
+                    return 'Your password needs to be at-least 6 characters';
+                  }
+                },
+                    onSaved: (input) => signupmodel.confirmPassword=input,
+                    decoration: InputDecoration(
+                        labelText: 'CONFIRM PASSWORD',
+                        labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                        // hintText: 'EMAIL',
+                        // hintStyle: ,
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green))),
+                            obscureText: true,
+                  ),
+                  SizedBox(height: 10.0),
+                  Container(
+                      height: 40.0,
+                      child: Material(
+                        borderRadius: BorderRadius.circular(20.0),
+                        shadowColor: Colors.greenAccent,
+                        color: Colors.green,
+                        elevation: 7.0,
+                        child: GestureDetector(
+                          onTap: save,
+                          child: Center(
+                            child: Text(
+                              'SIGNUP',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Montserrat'),
+                            ),
+                          ),
+                        ),
+                      )),
+                  SizedBox(height: 20.0),
+                  Container(
+                    height: 40.0,
+                    color: Colors.transparent,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Colors.black,
+                              style: BorderStyle.solid,
+                              width: 1.0),
+                          color: Colors.transparent,
+                          borderRadius: BorderRadius.circular(20.0)),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: 
+                        
+                            Center(
+                              child: Text('Go Back',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Montserrat')),
+                            ),
+                        
+                        
+                      ),
+                    ),
+                  ),
+                ],
+              )),
+          
+      )  ]));
+  }
+}
+
+class Signupmodel{
+  String name;
+  String email;
+  String password;
+  String confirmPassword;
+  Signupmodel({this.name,this.email,this.password,this.confirmPassword});
+} 
